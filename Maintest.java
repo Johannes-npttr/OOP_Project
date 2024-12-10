c965013af395d3dbbe7a80eaf12e5d214c07e13c
suelias <eliaskafitz@gmail.com>
2024-12-10 17:10:11 +0100

    test

--- Maintest.java
+++ Maintest.java
@@ -2,6 +2,9 @@ public class Maintest
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World");
+        for(int i = 0; i<10;++i)
+        {
+            System.out.println("Hello World"+ i);
+        }        
     }
 }
